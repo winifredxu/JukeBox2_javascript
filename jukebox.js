da76b8244ba7a46db6f81dc331726410be8cd16c
wxu <winnielandau@gmail.com>
2015-01-26 15:12:10 -0800

    enable Play button, and change text and disabled during play

--- jukebox.js
+++ jukebox.js
@@ -7,8 +7,23 @@
 var promptAndPlay = function() {
   var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
   var song = parseSong(songString);
-  playSong(song, 500, promptAndPlay);
+// WXU
+//  playSong(song, 500, promptAndPlay);
+	$('#play_button').attr('disabled', true);
+	$('#play_button').html("Playing...");
+	playSong(song, 500, changeDisplay );
 };
 
 // Get things going.
-promptAndPlay();
+// WXU:   
+// promptAndPlay();
+
+$(document).ready(function() {
+	$('#play_button').on('click', promptAndPlay);
+
+});
+
+var changeDisplay = function() {
+	$('#play_button').attr('disabled', false);
+	$('#play_button').html("To Play");
+};
